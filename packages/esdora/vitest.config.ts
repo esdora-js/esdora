f941c1eb2be06afeb8e04a5db49f26d4caee84ab
DreamyTZK <texon233@gmail.com>
2025-09-27 08:33:34 +0800

    test: 调整主包vitest配置文件

--- packages/esdora/vitest.config.ts
+++ packages/esdora/vitest.config.ts
@@ -1,35 +1,12 @@
+import { createTestConfig } from '@esdora/build-tools/configs'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
-  test: {
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'node_modules/',
-        'dist/',
-        '**/*.d.ts',
-        '**/*.config.*',
-      ],
-      thresholds: {
-        global: {
-          branches: 80,
-          functions: 80,
-          lines: 80,
-          statements: 80,
-        },
-      },
-    },
-    environment: 'node',
-    globals: true,
-    include: [
-      'test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-    ],
-    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
-    server: {
-      deps: {
-        inline: ['vitest-package-exports'],
-      },
-    },
+export default defineConfig(createTestConfig({
+  environment: 'node',
+  coverageThresholds: {
+    branches: 80,
+    functions: 80,
+    lines: 80,
+    statements: 80,
   },
-})
+}))
