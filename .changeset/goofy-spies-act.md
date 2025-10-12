c8b89c163ae6271b57dd73ebc9c320868ce2d388
Larry Walker <texon233@gmail.com>
2025-10-12 22:06:07 +0800

    fix(@esdora/date): 修正日期格式常量以匹配date-fns规范 (#156)

---
+++ .changeset/goofy-spies-act.md
@@ -0,0 +1,5 @@
+---
+"@esdora/date": patch
+---
+
+fix(@esdora/date): 修正日期格式常量以匹配date-fns规范
--- packages/date/src/constant/index.ts
+++ packages/date/src/constant/index.ts
@@ -1,6 +1,10 @@
+/**
+ * 日期格式常量
+ * 参考：https://date-fns.org/v4.1.0/docs/format
+ */
 export const DATE_FORMAT = {
   HH_MM_SS: 'HH:mm:ss',
-  YYYY_MM_DD: 'YYYY-MM-DD',
-  YYYY_MM_DD_HH_MM: 'YYYY-MM-DD HH:mm',
-  YYYY_MM_DD_HH_MM_SS: 'YYYY-MM-DD HH:mm:ss',
+  YYYY_MM_DD: 'yyyy-MM-dd',
+  YYYY_MM_DD_HH_MM: 'yyyy-MM-dd HH:mm',
+  YYYY_MM_DD_HH_MM_SS: 'yyyy-MM-dd HH:mm:ss',
 }
